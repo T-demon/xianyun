1e927b17d5ca05130d54f6657b0e1cd248b11ac0
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-11 20:06:22 +0800

    轮播图

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,87 @@
 <template>
   <div>
-    
-    <el-carousel :interval="5000" arrow="always">
-      <el-carousel-item v-for="item in 4" :key="item">
-        
-      </el-carousel-item>
-    </el-carousel>
+    <!-- 轮播图 -->
+    <div class="container">
+      <el-carousel :interval="5000" arrow="always">
+        <el-carousel-item v-for="(item,index) in banners" :key="index">
+          <div
+            class="banner-image"
+            :style="`
+                background:url(${$axios.defaults.baseURL+item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+          ></div>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: index === currentOption}"
+            @click="handleOption(index)"
+          >
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+            :placeholder="options[currentOption].placeholder"
+            v-model="searchValue"
+            @keyup.enter="handleSearch"
+          />
+          <i class="el-icon-search" @click="handleSearch"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      banners: [],
+      option: []
+    };
+  },
+  methods: {
+    handleSearch() {}
+  },
 
-  mounted(){
+  mounted() {
     this.$axios({
-      url:"/scenics/banners",
-    }).then(res=>{
-      console.log(res)
-    })
+      method: "GET",
+      url: "/scenics/banners"
+    }).then(res => {
+      const { data } = res.data;
+      this.banners = data;
+      // console.log(this.banners)
+    });
   }
 };
 </script>
 
 
-<style>
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 18px;
-    opacity: 0.75;
-    line-height: 300px;
-    margin: 0;
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
+
+  /deep/ .el-carousel__container {
+    height: 700px;
   }
-  
-  .el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
   }
-  
-  .el-carousel__item:nth-child(2n+1) {
-    background-color: #d3dce6;
-  }
+}
 </style>
