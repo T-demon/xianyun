9eb76e94b8d8478eb1798916ee833a45b0c520d6
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-11 19:14:31 +0800

    分离头部和底部为公共组件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
+    <Header/>
+
+    <!-- 内容占位组件 -->
     <nuxt />
+
+    <Footer/>
+
   </div>
 </template>
+
+
+<script>
+import Header from "@/components/header"
+import Footer from "@/components/footer"
+
+export default {
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
 
 <style>
 html {
