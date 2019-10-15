ee977dbc00468659802b31d8239ca62d18b5d6ec
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-15 14:35:18 +0800

    实现机票列表分页功能

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -13,10 +13,25 @@
 
         <!-- 航班信息 -->
         <div>
-          <FlightsItem 
-          v-for="(item,index) in dataList" :key="index"
-          :data="item"
-          />
+          <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+
+          <!-- 分页 -->
+          <el-row type="flex" justify="center" style="margin-top:10px;">
+            <!-- size-change：切换条数时候触发 -->
+            <!-- current-change：选择页数时候触发 -->
+            <!-- current-page: 当前页数 -->
+            <!-- page-size：当前条数 -->
+            <!-- total：总条数 -->
+            <el-pagination
+              @size-change="handleSizeChange"
+              @current-change="handleCurrentChange"
+              :current-page="pageIndex"
+              :page-sizes="[5, 10, 15, 20]"
+              :page-size="pageSize"
+              layout="total, sizes, prev, pager, next, jumper"
+              :total="flightsData.total"
+            ></el-pagination>
+          </el-row>
         </div>
       </div>
 
@@ -37,8 +52,13 @@ import moment from "moment";
 export default {
   data() {
     return {
-      flightsData: [],
-      dataList: []
+      flightsData: {
+        flights: []
+      },
+   
+
+      pageIndex: 1,
+      pageSize: 5
     };
   },
 
@@ -52,11 +72,31 @@ export default {
       url: "airs",
       params: this.$route.query
     }).then(res => {
-        console.log(res.data);
-        
+      console.log(res.data);
       this.flightsData = res.data;
-      this.dataList = this.flightsData.flights;
     });
+  },
+
+  methods: {
+    //   分页条数切换时触发
+    handleSizeChange(val) {
+      this.pageSize = val;
+    },
+
+    // 分页页数切换
+    handleCurrentChange(val) {
+      this.pageIndex = val;
+    }
+  },
+
+  computed: {
+    dataList(){
+        const arr = this.flightsData.flights.slice(
+            (this.pageIndex-1)*this.pageSize,
+            this.pageIndex*this.pageSize
+        );
+        return arr
+    }
   }
 };
 </script>
